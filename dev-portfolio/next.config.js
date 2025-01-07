eba144e310309e32187c8ecc873c971d2a8f31e3
jihyunkim329 <124066467+jihyunkim-dev@users.noreply.github.com>
2025-01-07 23:20:55 +0900

    next.config.js 설정

---
+++ dev-portfolio/next.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  output: "standalone",
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+      },
+    ],
+  },
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
+};
