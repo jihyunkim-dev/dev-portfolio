39a801aa6931f80b1442c5d08bfba9a502bbf064
jihyunkim329 <124066467+jihyunkim-dev@users.noreply.github.com>
2025-01-13 00:47:20 +0900

    Wave Effect 생성

--- dev-portfolio/src/containers/Main/MainClientBlock.js
+++ dev-portfolio/src/containers/Main/MainClientBlock.js
@@ -2,43 +2,88 @@
 
 import { styled, keyframes } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
+import theme from "@/configs/customTheme";
 
-const wave = keyframes({});
+const waveAnimation = keyframes({
+  from: {
+    transform: "rotate(0deg)",
+  },
+  to: {
+    transform: "rotate(360deg)",
+  },
+});
+
+const WaveEffectGridContainer = styled(Grid)({
+  display: "flex",
+  position: "relative",
+  width: "100vw",
+  height: "100vh",
+  justifyContent: "center",
+  alignItems: "center",
+  overflow: "hidden",
+});
 
 const FirstWaveBlock = styled("div")({
   position: "absolute",
-
+  top: "50%",
+  left: "-50%",
   width: "200vw",
   height: "200vw",
-  borderRadius: "40%",
-  backgroundColor: "#000000",
+  borderRadius: "38%",
+  backgroundColor: theme.palette.primary[200],
+  animation: `${waveAnimation} 15000ms infinite linear`,
+  opacity: 0.6,
+});
+
+const SecondWaveBlock = styled("div")({
+  position: "absolute",
+  top: "65%",
+  left: "-50%",
+  width: "220vw",
+  height: "220vw",
+  borderRadius: "38%",
+  backgroundColor: theme.palette.primary[800],
+  animation: `${waveAnimation} 40000ms infinite linear`,
+  opacity: 0.4,
+});
+
+const ThirdWaveBlock = styled("div")({
+  position: "absolute",
+  top: "90%",
+  left: "-40%",
+  width: "200vw",
+  height: "200vw",
+  borderRadius: "30%",
+  backgroundColor: theme.palette.primary[500],
+  animation: `${waveAnimation} 50000ms infinite linear`,
+  opacity: 0.8,
 });
 
 function FirstWave() {
-  return (
-    <div>
-      <FirstWaveBlock />
-    </div>
-  );
+  return <FirstWaveBlock />;
 }
 
 function SecondWave() {
-  return <Grid container></Grid>;
+  return <SecondWaveBlock />;
 }
 function ThirdWave() {
-  return <Grid container></Grid>;
+  return <ThirdWaveBlock />;
 }
 
 export function WaveEffect() {
   return (
-    <Grid container>
+    <WaveEffectGridContainer container>
       <Grid container item>
         <Grid item>
           <FirstWave />
         </Grid>
+        <Grid item>
+          <SecondWave />
+        </Grid>
+        <Grid item>
+          <ThirdWave />
+        </Grid>
       </Grid>
-      <SecondWave />
-      <ThirdWave />
-    </Grid>
+    </WaveEffectGridContainer>
   );
 }
--- dev-portfolio/src/containers/Main/MainServerBlock.js
+++ dev-portfolio/src/containers/Main/MainServerBlock.js
@@ -2,7 +2,7 @@ import { WaveEffect } from "./MainClientBlock";
 
 export default function MainServerBlock() {
   return (
-    <div className="w-full">
+    <div className="w-full h-[full] overflow-hidden">
       <WaveEffect />
     </div>
   );
