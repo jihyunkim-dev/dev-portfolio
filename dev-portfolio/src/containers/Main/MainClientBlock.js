dd07d57c11b37e7da4a2deebbcc38a18680fb50f
jihyunkim329 <124066467+jihyunkim-dev@users.noreply.github.com>
2025-01-15 00:23:29 +0900

    Wave Effect 수정

--- dev-portfolio/src/containers/Main/MainClientBlock.js
+++ dev-portfolio/src/containers/Main/MainClientBlock.js
@@ -4,18 +4,13 @@ import { styled, keyframes } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import theme from "@/configs/customTheme";
 
-// const waveAnimation = keyframes({
-//   from: {
-//     transform: "rotate(0deg)",
-//   },
-//   to: {
-//     transform: "rotate(360deg)",
-//   },
-// });
-
 const waveAnimation = keyframes({
-  "0%, 100%": { transform: "translateY(0)" },
-  "50%": { transform: "translateY(-20px)" },
+  from: {
+    transform: "rotate(0deg)",
+  },
+  to: {
+    transform: "rotate(360deg)",
+  },
 });
 
 const WaveEffectGridContainer = styled(Grid)({
@@ -31,6 +26,7 @@ const WaveEffectGridContainer = styled(Grid)({
 
 const WaveBase = styled("div")({
   position: "absolute",
+  zIndex: 10,
   left: "-100%",
   width: "300%",
   height: "100%",
@@ -38,66 +34,57 @@ const WaveBase = styled("div")({
   animation: `${waveAnimation} 10s ease-in-out infinite`,
 });
 
-const FirstWaveBlock = styled(WaveBase)({
-  bottom: "-65%",
-  background: "linear-gradient(180deg, transparent 50%, #BD92E8 50%)",
-  animationDelay: "0s",
+const FirstWaveBlock = styled("div")({
+  position: "absolute",
+  top: "70%",
+  left: "-20%",
+  width: "200vw",
+  height: "200vw",
+  borderRadius: "38%",
+  background: "linear-gradient(70deg, #E0CCF6, #BD92E8)",
+  animation: `${waveAnimation} 18000ms infinite linear`,
+  opacity: 0.7,
+  filter: "blur(3px)",
 });
 
-const SecondWaveBlock = styled(WaveBase)({
-  bottom: "-70%",
-  background: "linear-gradient(180deg, transparent 50%, #7D48AC 50%)",
-  animationDelay: "-3s",
+const SecondWaveBlock = styled("div")({
+  position: "absolute",
+  top: "35%",
+  left: "-80%",
+  width: "220vw",
+  height: "220vw",
+  borderRadius: "38%",
+  background: "linear-gradient(45deg, #7D48AC, #6B2DA0)",
+  animation: `${waveAnimation} 30000ms infinite linear`,
+  opacity: 0.3,
+  filter: "blur(3px)",
+});
+
+const ThirdWaveBlock = styled("div")({
+  position: "absolute",
+  top: "20%",
+  left: "0%",
+  width: "200vw",
+  height: "200vw",
+  borderRadius: "30%",
+  backgroundColor: "linear-gradient(45deg, #BD92E8, #6B2DA0)",
+  animation: `${waveAnimation} 30000ms infinite linear`,
   opacity: 0.5,
+  filter: "blur(3px)",
 });
 
-// const FirstWaveBlock = styled("div")({
-//   position: "absolute",
-//   top: "70%",
-//   left: "-20%",
-//   width: "200vw",
-//   height: "200vw",
-//   borderRadius: "38%",
-//   background: "linear-gradient(70deg, #E0CCF6, #BD92E8)",
-//   animation: `${waveAnimation} 18000ms infinite linear`,
-//   opacity: 0.7,
-// });
-
-// const SecondWaveBlock = styled("div")({
-//   position: "absolute",
-//   top: "85%",
-//   left: "-80%",
-//   width: "320vw",
-//   height: "220vw",
-//   borderRadius: "38%",
-//   background: "linear-gradient(45deg, #7D48AC, #7d48ac)",
-//   animation: `${waveAnimation} 30000ms infinite linear`,
-//   opacity: 0.3,
-// });
-
-// const ThirdWaveBlock = styled("div")({
-//   position: "absolute",
-//   top: "50%",
-//   left: "-40%",
-//   width: "200vw",
-//   height: "200vw",
-//   borderRadius: "30%",
-//   backgroundColor: "#CBAAED",
-//   animation: `${waveAnimation} 30000ms infinite linear`,
-//   opacity: 0.5,
-// });
-
-// const FourthWaveBlock = styled("div")({
-//   position: "absolute",
-//   top: "20%",
-//   left: "-20%",
-//   width: "400vw",
-//   height: "400vw",
-//   borderRadius: "30%",
-//   background: "linear-gradient(45deg, #E0CCF6, #45156A)",
-//   animation: `${waveAnimation} 50000ms infinite linear`,
-//   opacity: 0.3,
-// });
+const FourthWaveBlock = styled("div")({
+  position: "absolute",
+  top: "20%",
+  left: "-20%",
+  width: "400vw",
+  height: "400vw",
+  borderRadius: "30%",
+  background: "linear-gradient(45deg, #E0CCF6, #45156A)",
+  animation: `${waveAnimation} 50000ms infinite linear`,
+  opacity: 0.3,
+  filter: "blur(2px)",
+});
 
 // const FifthWaveBlock = styled("div")({
 //   position: "absolute",
@@ -118,13 +105,13 @@ function FirstWave() {
 function SecondWave() {
   return <SecondWaveBlock />;
 }
-// function ThirdWave() {
-//   return <ThirdWaveBlock />;
-// }
+function ThirdWave() {
+  return <ThirdWaveBlock />;
+}
 
-// function FourthWave() {
-//   return <FourthWaveBlock />;
-// }
+function FourthWave() {
+  return <FourthWaveBlock />;
+}
 
 // function FifthWave() {
 //   return <FifthWaveBlock />;
@@ -140,8 +127,12 @@ export function WaveEffect() {
         <Grid item>
           <SecondWave />
         </Grid>
-        <Grid item>{/* <ThirdWave /> */}</Grid>
-        <Grid item>{/* <FourthWave /> */}</Grid>
+        <Grid item>
+          <ThirdWave />
+        </Grid>
+        <Grid item>
+          <FourthWave />
+        </Grid>
         <Grid item>{/* <FifthWave /> */}</Grid>
       </Grid>
     </WaveEffectGridContainer>
