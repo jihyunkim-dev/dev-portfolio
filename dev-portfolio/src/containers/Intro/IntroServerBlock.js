ef459828ada9f88b84bd6f11117cc548d3b7994c
jihyunkim329 <124066467+jihyunkim-dev@users.noreply.github.com>
2025-01-21 23:57:51 +0900

    인트로 1,2 배치

--- dev-portfolio/src/containers/Intro/IntroServerBlock.js
+++ dev-portfolio/src/containers/Intro/IntroServerBlock.js
@@ -7,6 +7,12 @@ export default function IntroServerBlock() {
         <div className="relative z-20 w-full h-full flex flex-col items-center justify-center">
           <IntroTitle />
           <IntroDescription />
+          <div className="w-full flex justify-start">
+            <FirstIntro />
+          </div>
+          <div className="w-full flex justify-start">
+            <SecondIntro />
+          </div>
         </div>
       </div>
     </DefaultLayout>
@@ -17,7 +23,7 @@ function IntroTitle() {
   return (
     <div className="flex pt-[200px]">
       <p className="text-[80px] font-[NanumSquareNeoExtraBold] text-primary-700 tracking-wider">
-        REPEATER
+        REPEATER.
       </p>
     </div>
   );
@@ -26,9 +32,25 @@ function IntroTitle() {
 function IntroDescription() {
   return (
     <div className="flex ">
-      <p className="text-[30px] font-[SpoqaHanSansNeoBold] text-darkGray-700 tracking-widest">
-        Repeater와 같은 특징을 지닌 Developer.
+      <p className="text-[20px] font-[NanumSquareNeoExtraBold] text-darkGray-700 tracking-widest">
+        아이디어로 가능성을 증폭시키고, 코드로 가능성을 확장하는 Developer.
       </p>
+    </div>
+  );
+}
+
+function FirstIntro() {
+  return (
+    <div>
+      <p>Test1</p>
+    </div>
+  );
+}
+
+function SecondIntro() {
+  return (
+    <div>
+      <p>Test2</p>
     </div>
   );
 }
