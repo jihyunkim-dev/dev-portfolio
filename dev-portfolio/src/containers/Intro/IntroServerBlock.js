17ded86f37461883baeb144e56795feb558d6d80
jihyunkim329 <124066467+jihyunkim-dev@users.noreply.github.com>
2025-01-22 01:23:04 +0900

    Intro Description 생성

--- dev-portfolio/src/containers/Intro/IntroServerBlock.js
+++ dev-portfolio/src/containers/Intro/IntroServerBlock.js
@@ -31,10 +31,29 @@ function IntroTitle() {
 
 function IntroDescription() {
   return (
-    <div className="flex ">
-      <p className="text-[20px] font-[NanumSquareNeoExtraBold] text-darkGray-700 tracking-widest">
-        아이디어로 가능성을 증폭시키고, 코드로 가능성을 확장하는 Developer.
-      </p>
+    <div className="">
+      <div className="flex justify-center">
+        <p className="text-[20px] font-[NanumSquareNeoExtraBold] text-darkGray-700 tracking-widest">
+          아이디어로 가능성을
+        </p>
+        <p className="text-[20px] font-[NanumSquareNeoExtraBold] text-primary-700 tracking-widest">
+          &nbsp;증폭
+        </p>
+        <p className="text-[20px] font-[NanumSquareNeoExtraBold] text-darkGray-700 tracking-widest">
+          시키고
+        </p>
+      </div>
+      <div className="flex">
+        <p className="text-[20px] font-[NanumSquareNeoExtraBold] text-darkGray-700 tracking-widest">
+          코드로 가능성을
+        </p>
+        <p className="text-[20px] font-[NanumSquareNeoExtraBold] text-primary-700 tracking-widest">
+          &nbsp;확장
+        </p>
+        <p className="text-[20px] font-[NanumSquareNeoExtraBold] text-darkGray-700 tracking-widest">
+          하는 Developer.
+        </p>
+      </div>
     </div>
   );
 }
