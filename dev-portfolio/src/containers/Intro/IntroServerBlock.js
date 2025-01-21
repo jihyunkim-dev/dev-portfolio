a37e21425ab09d132f67811056d0c8f5abfa6cdb
jihyunkim329 <124066467+jihyunkim-dev@users.noreply.github.com>
2025-01-21 23:47:09 +0900

    Intro Title 생성

--- dev-portfolio/src/containers/Intro/IntroServerBlock.js
+++ dev-portfolio/src/containers/Intro/IntroServerBlock.js
@@ -5,9 +5,30 @@ export default function IntroServerBlock() {
     <DefaultLayout>
       <div className="relative w-full h-full min-h-screen">
         <div className="relative z-20 w-full h-full flex flex-col items-center justify-center">
-          <div className="text-primary-900">Intro Test</div>
+          <IntroTitle />
+          <IntroDescription />
         </div>
       </div>
     </DefaultLayout>
+  );
+}
+
+function IntroTitle() {
+  return (
+    <div className="flex pt-[200px]">
+      <p className="text-[80px] font-[NanumSquareNeoExtraBold] text-primary-700 tracking-wider">
+        REPEATER
+      </p>
+    </div>
+  );
+}
+
+function IntroDescription() {
+  return (
+    <div className="flex ">
+      <p className="text-[30px] font-[SpoqaHanSansNeoBold] text-darkGray-700 tracking-widest">
+        Repeater와 같은 특징을 지닌 Developer.
+      </p>
+    </div>
   );
 }
