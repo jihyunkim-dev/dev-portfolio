5ea5c483d495298785a3e2953583335280aa6070
jihyunkim329 <124066467+jihyunkim-dev@users.noreply.github.com>
2025-03-06 00:33:45 +0900

    두번째 시그널 생성

--- dev-portfolio/src/containers/Intro/IntroClientBlock.js
+++ dev-portfolio/src/containers/Intro/IntroClientBlock.js
@@ -4,39 +4,94 @@ import theme from "@/configs/customTheme";
 import Grid from "@mui/material/Grid";
 import { styled, keyframes } from "@mui/material/styles";
 
-const LargeSignal = styled("div")({
+const FirstLargeSignal = styled("div")({
   display: "flex",
-  position: "absolute",
+  position: "relative",
   width: "200px",
   height: "200px",
   borderRadius: "50%",
   background: "#1CB0FF",
+  opacity: 0.3,
+  filter: "blur(10px)",
+  flexShrink: 0,
 });
 
-const MediumSignal = styled("div")({
+const FirstMediumSignal = styled("div")({
   display: "flex",
   position: "absolute",
   width: "150px",
   height: "150px",
   borderRadius: "50%",
   background: "#1CB0FF",
+  opacity: 0.55,
+  filter: "blur(9px)",
+  flexShrink: 0,
 });
 
-const SmallSignal = styled("div")({
+const FirstSmallSignal = styled("div")({
   display: "flex",
   position: "absolute",
-  width: "100px",
-  height: "100px",
+  width: "80px",
+  height: "80px",
   borderRadius: "50%",
   background: "#1CB0FF",
+  opacity: 0.75,
+  filter: "blur(7px)",
+  flexShrink: 0,
 });
 
-function Signal() {
+const SecondLargeSignal = styled("div")({
+  display: "flex",
+  position: "relative",
+  width: "150px",
+  height: "150px",
+  borderRadius: "50%",
+  background: "#1CB0FF",
+  opacity: 0.3,
+  filter: "blur(10px)",
+  flexShrink: 0,
+});
+
+const SecondMediumSignal = styled("div")({
+  display: "flex",
+  position: "absolute",
+  width: "80px",
+  height: "80px",
+  borderRadius: "50%",
+  background: "#1CB0FF",
+  opacity: 0.55,
+  filter: "blur(9px)",
+  flexShrink: 0,
+});
+
+const SecondSmallSignal = styled("div")({
+  display: "flex",
+  position: "absolute",
+  width: "40px",
+  height: "40px",
+  borderRadius: "50%",
+  background: "#1CB0FF",
+  opacity: 0.75,
+  filter: "blur(7px)",
+  flexShrink: 0,
+});
+
+function FirstSignal() {
   return (
-    <Grid container>
-      <LargeSignal />
-      <MediumSignal />
-      <SmallSignal />
+    <Grid container justifyContent="center" alignItems="center">
+      <FirstLargeSignal />
+      <FirstMediumSignal />
+      <FirstSmallSignal />
+    </Grid>
+  );
+}
+
+function SecondSignal() {
+  return (
+    <Grid container justifyContent="center" alignItems="center">
+      <SecondLargeSignal />
+      <SecondMediumSignal />
+      <SecondSmallSignal />
     </Grid>
   );
 }
@@ -44,7 +99,14 @@ function Signal() {
 export function SignalEffect() {
   return (
     <Grid container>
-      <Signal />
+      <Grid container justifyContent="center" alignItems="center" spacing={1}>
+        <Grid item xs={6} justifyContent="flex-start">
+          <FirstSignal />
+        </Grid>
+        <Grid item xs={4} justifyContent="flex-start">
+          <SecondSignal />
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
